26df527c8de43aa6ef0e0da729519f424f36394a
unknown <aneleninst@gmail.com>
2025-10-22 12:28:38 +0500

    + выполнение первого задания

---
+++ Задание 1. Классы/Задание 1. Классы/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Author
+{
+    public string Name;
+    public DateTime RegistrationDate = DateTime.UtcNow;
+    public List<Note> Notes;
+}
+
+public class Note
+{
+    public string BookName;
+    public string Description;
+    public DateTime DateOfCreation = DateTime.UtcNow;
+    public bool IsExecuted;
+    public Author Author;
+}
