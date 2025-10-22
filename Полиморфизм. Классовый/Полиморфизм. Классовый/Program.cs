8953c6996d2cce3917158ee435ae69e094ac8dea
unknown <aneleninst@gmail.com>
2025-10-22 16:02:08 +0500

    + сделала первуое задание по полиморфизму!

---
+++ Полиморфизм. Классовый/Полиморфизм. Классовый/Program.cs
@@ -0,0 +1,39 @@
+﻿var animals = new List<Animal>
+{
+    new Fox(),
+    new Wolf(),
+    new Rabbit(),
+};
+foreach (var animal in animals)
+{
+    animal.Run();
+}
+
+public abstract class Animal
+{
+    public virtual void Run()
+    {
+        Console.WriteLine("I am animal. I am running for my life!");
+    }
+}
+
+public class Rabbit : Animal
+{
+    public override void Run()
+    {
+        Console.WriteLine("I am running from predators");
+    }
+}
+
+public class Wolf : Animal
+{
+    public override void Run()
+    {
+        Console.WriteLine("I am running for tasty rabbit ;)");
+    }
+}
+
+public class Fox : Animal
+{
+    
+}
