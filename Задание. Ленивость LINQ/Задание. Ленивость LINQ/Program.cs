b2b794d8b04c1a9e76ce629161f505db74164253
unknown <aneleninst@gmail.com>
2025-10-23 18:02:48 +0500

    Сделала задание по ленивости

---
+++ Задание. Ленивость LINQ/Задание. Ленивость LINQ/Program.cs
@@ -0,0 +1,44 @@
+﻿List<Person> persons = new()
+{
+    new() { FirstName = "Виталий", LastName = "Цаль", Age = 33 },
+    new() { FirstName = "Куджо", LastName = "Джотаро", Age = 40 },
+    new() { FirstName = "Юрий", LastName = "Хованский", Age = 34 },
+    new() { FirstName = "Михаил", LastName = "Петров", Age = 15 },
+    new() { FirstName = "Виталий", LastName = "Гачиев", Age = 40 },
+    new() { FirstName = "Юрий", LastName = "Гагарин", Age = 34 },
+};
+
+var filterd = FilterPersons(persons, "Виталий",null,40);
+Console.WriteLine(filterd.Count());
+
+
+
+
+
+List<Person> FilterPersons(List<Person> all, string firstName = null, string lastName = null, int age = 0)
+{
+    IEnumerable<Person> filterPersons = all;
+    if (firstName != null)
+    {
+        filterPersons = filterPersons.Where(person => person.FirstName.Contains(firstName));
+    }
+
+    if (lastName != null)
+    {
+        filterPersons = filterPersons.Where(person => person.LastName.Contains(lastName));
+    }
+
+    if (age != 0)
+    {
+        filterPersons = filterPersons.Where(person => person.Age == age);
+    }
+    return filterPersons.ToList();
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    
+}
