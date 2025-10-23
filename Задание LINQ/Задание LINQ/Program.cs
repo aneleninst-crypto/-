050480d5aaa599dd1af3df3e728a2475085fd832
unknown <aneleninst@gmail.com>
2025-10-23 12:16:10 +0500

    Сделала задания LINQ!

---
+++ Задание LINQ/Задание LINQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+List<string> names = new()
+{
+    "Миша",
+    "Вася",
+    "Петя",
+    "Гриша"
+};
+
+var firstName = names.First();
+Console.WriteLine(firstName);
+
+
+var filteredNames = names.Skip(1).Take(2);
+foreach (var name in  filteredNames )
+{
+    Console.WriteLine(name);
+}
+
+var filteredNames1 = names.Where(name => name.StartsWith("М"));
+foreach (var name in filteredNames1)
+{
+    Console.WriteLine(name);
+}
+
+var filteredNames2 = names.Where(name => name.Contains("я"));
+foreach (var name in  filteredNames2)
+{
+    Console.WriteLine(name);
+}
+
+var filteredNames3 = names.Where(name => name.Contains("я")).Count();
+Console.WriteLine(filteredNames3);
+
+var filteredNames4 = names.Contains("Петя");
+Console.WriteLine(filteredNames4);
+
+var filteredNames5 = names.OrderBy(name => name);
+foreach (var name in filteredNames5)
+{
+    Console.WriteLine(name);
+}
