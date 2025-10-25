b9297cd563f40886169ed8760cd4e307f26dbdfe
unknown <aneleninst@gmail.com>
2025-10-25 16:11:11 +0500

    Задание замыкание

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿var funcs = new List<Func<int, int>>();
+for (int i = 0; i < 10; i++)
+{
+    int number = i;
+    funcs.Add(x => x + number);
+}
+
+foreach(var func in funcs)
+    Console.WriteLine(func(1));
