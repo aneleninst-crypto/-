fe24be760cf6a2b4a8aa2dc57a5c61807783338a
unknown <aneleninst@gmail.com>
2025-10-26 12:18:45 +0500

    Две последние задачки

---
+++ IEnumerable/IEnumerable/Program.cs
@@ -0,0 +1,21 @@
+﻿HashSet<A> @set = new();
+var el1 = new A {Value = 1};
+var el2 = new A {Value = 1};
+@set.Add(el1);
+Console.WriteLine(@set.Contains(el2));
+struct A
+{
+    public int Value { get; set; }
+    public override int GetHashCode()
+    {
+        Console.WriteLine("GetHashCode");
+        return Value;
+    }
+
+    // public override bool Equals(object? obj)
+    // {
+    //     if (obj is A other)
+    //         return Value == other.Value;
+    //     return false;
+    // }
+}
---
+++ Неопределенность/Неопределенность/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<B, string> map = new();
+var el1 = new B();
+var el2 = new B();
+map[el1] = "12";
+map[el2] = "14";
+Console.WriteLine(map[el1]);
+class B
+{
+    private static readonly Random _random = new();
+    private readonly int _hashCode;
+    public B()
+    {
+        _hashCode = _random.Next();
+    }
+    public override int GetHashCode() => _hashCode;
+    public override bool Equals(object obj) => true;
+}
