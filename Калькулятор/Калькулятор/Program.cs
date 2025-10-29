3e02c963cd23a02d9624e913038fd093f4f4e2da
unknown <aneleninst@gmail.com>
2025-10-29 14:24:28 +0500

    Я сделала Калькулятор

---
+++ Калькулятор/Калькулятор/Program.cs
@@ -0,0 +1,88 @@
+﻿ShowInstructions();
+while (true)
+{
+    var input = Console.ReadLine();
+if (input.ToLower() == "exit")
+{
+    break;
+}
+if (input.ToLower() == "help")
+{
+    ShowInstructions();
+}
+else 
+Console.WriteLine(ProcessUserInput(input));
+}
+
+static double ProcessUserInput(string input)
+{
+var numbers = ParseExpression(input);
+var operand1 = double.Parse(numbers[0]);
+var operand2 = double.Parse(numbers[2]);
+var operatorr = char.Parse(numbers[1]);
+
+var result = FunctionSelector(operand1,operand2,operatorr);
+return result;
+}
+
+static string[] ParseExpression(string input)
+{
+    return input.Trim().Split(" ");
+}
+
+static double FunctionSelector(double operand1, double operand2, char operatorr)
+{
+    switch (operatorr)
+    {
+        case '+':
+            return Add(operand1, operand2);
+        case '-':
+            return Subtraction(operand1, operand2);
+        case '*':
+            return Multiplication(operand1, operand2);
+        case '/':
+            return Divide(operand1, operand2);
+        case '%':
+            return Modulo(operand1, operand2);
+        case '^' :
+            return Exponentiation(operand1, operand2);
+        default:
+            throw new ArgumentException("Неизвестный оператор");
+    }
+}
+
+static double Divide (double dividend, double divider)
+{
+return dividend / divider;
+}
+
+static double Add(double dividend, double divider)
+{
+    return dividend + divider;
+}
+
+static double Subtraction(double dividend, double divider)
+{
+return dividend - divider;
+}
+
+static double Multiplication(double dividend, double divider)
+{
+    return dividend * divider;
+}
+
+static double Modulo(double dividend, double divider)
+{
+    return dividend % divider;
+}
+
+static double Exponentiation(double dividend, double divider)
+{
+    return Math.Pow(dividend, divider);
+}
+static void ShowInstructions()
+{
+    Console.WriteLine("Привет! Это иструкция: введите выражение в формате число-знак-число. " +
+                  "В качестве знаков используйте: +, -, *, /, %, ^. Команды: Help - вызвать иструкцию, " +
+                  "команда Exit - выйти из программы.\n");
+}
