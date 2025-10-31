7cd12d1f3f53bf610220370050bad6eca4c8671d
unknown <aneleninst@gmail.com>
2025-10-31 12:49:45 +0500

    Новые правки, использование логгера

--- Калькулятор/Калькулятор/Program.cs
+++ Калькулятор/Калькулятор/Program.cs
@@ -1,89 +1,140 @@
-﻿ShowInstructions();
+﻿using Логгер;
+
+ConsoleLogger consoleLogger = new();
+FileLogger fileLogger = new();
+var listLoggers = new List<ILogger>
+{
+    consoleLogger,
+    fileLogger
+};
+var compositeLogger = new CompositeLogger(listLoggers);
+
+Calculator calculator = new(compositeLogger);
+
+ShowInstructions();
+
 while (true)
 {
+
     var input = Console.ReadLine();
-if (input.ToLower() == "exit")
-{
-    break;
-}
-if (input.ToLower() == "help")
-{
-    ShowInstructions();
-}
-else 
-Console.WriteLine(ProcessUserInput(input));
-}
-
-static double ProcessUserInput(string input)
-{
-var numbers = ParseExpression(input);
-var operand1 = double.Parse(numbers[0]);
-var operand2 = double.Parse(numbers[2]);
-var operatorr = char.Parse(numbers[1]);
-
-var result = FunctionSelector(operand1,operand2,operatorr);
-return result;
-}
-
-static string[] ParseExpression(string input)
-{
-    return input.Trim().Split(" ");
-}
-
-
-static double FunctionSelector (double operand1, double operand2, char operatorr)
-{ 
-    Dictionary<char, Func<double, double, double>> operations = new Dictionary<char, Func<double, double, double>>
+    if (input?.ToLower() == "exit")
     {
-        { '+', Add },
-        { '-', Subtraction },
-        { '*', Multiplication },
-        { '/', Divide },
-        { '%', Modulo },
-        { '^', Exponentiation },
-    }; 
-    if (operations.TryGetValue(operatorr, out var operation))
-    {
-        return operation(operand1, operand2);
+        break;
     }
-    else
+
+    if (input?.ToLower() == "help")
     {
-            throw new ArgumentException("Неизвестный оператор");
+        ShowInstructions();
     }
+    else 
+        try
+        {
+            if (input != null) 
+                Console.WriteLine(calculator.ProcessUserInput(input));
+        }
+        catch (DivideByZeroException byZeroException)
+        {
+            consoleLogger.LogError(byZeroException);
+        }
+        catch (Exception ex)
+        {
+            consoleLogger.LogError(ex);
+        }
 }
 
-static double Divide (double dividend, double divider)
-{
-return dividend / divider;
-}
-
-static double Add(double dividend, double divider)
-{
-    return dividend + divider;
-}
-
-static double Subtraction(double dividend, double divider)
-{
-return dividend - divider;
-}
-
-static double Multiplication(double dividend, double divider)
-{
-    return dividend * divider;
-}
-
-static double Modulo(double dividend, double divider)
-{
-    return dividend % divider;
-}
-
-static double Exponentiation(double dividend, double divider)
-{
-    return Math.Pow(dividend, divider);
-}
 static void ShowInstructions()
 {
-    Console.WriteLine("Привет! Это иструкция: введите выражение в формате число-знак-число. " +
-                  "В качестве знаков используйте: +, -, *, /, %, ^. Команды: Help - вызвать иструкцию, " +
+    Console.WriteLine("Привет!\nЭто иструкция!\nВведите выражение в формате число-знак-число. \n" +
+                  "В качестве знаков используйте: +, -, *, /, %, ^. \nКоманды: Help - вызвать иструкцию, " +
                   "команда Exit - выйти из программы.\n");
+}
+
+public class Calculator
+{
+    private readonly ILogger _logger;
+
+    public Calculator (ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public double ProcessUserInput(string input)
+    {
+        var numbers = ParseExpression(input);
+        var operand1 = double.Parse(numbers[0]);
+        var operand2 = double.Parse(numbers[2]);
+        var operatorr = char.Parse(numbers[1]);
+
+        return FunctionSelector(operand1,operand2,operatorr);
+    }
+
+    static string[] ParseExpression(string input)
+    {
+        return input.Trim().Split(" ");
+    }
+
+    private double FunctionSelector (double operand1, double operand2, char operatorr)
+    { 
+        var operations = new Dictionary<char, Func<double, double, double>>
+        {
+            { '+', Add },
+            { '-', Subtraction },
+            { '*', Multiplication },
+            { '/', Divide },
+            { '%', Modulo },
+            { '^', Exponentiation },
+        }; 
+        if (operations.TryGetValue(operatorr, out var operation))
+        {
+            return operation(operand1, operand2);
+        }
+        throw new ArgumentException("Неизвестный оператор");
+    }  
+    
+    private double Add(double dividend, double divider)
+    {
+        var result = dividend + divider;
+        _logger.LogInformation($"Сложение: {dividend} + {divider} = {result}");
+        return result;
+    }
+
+    private double Subtraction(double dividend, double divider)
+    {
+        var result = dividend - divider;
+        _logger.LogInformation($"Вычитание: {dividend} - {divider} = {result}");
+        return result;
+    }
+
+    private double Divide(double dividend, double divider)
+    {
+        if (divider == 0)
+        {
+            var ex = new DivideByZeroException("Невозможно делить на ноль");
+            throw ex;
+        }
+        var result = dividend / divider;
+        _logger.LogInformation($"Деление: {dividend} / {divider} = {result}");
+        return result;
+    }
+
+    private double Multiplication(double dividend, double divider)
+    {
+        var result = dividend * divider;
+        _logger.LogInformation($"Умножение: {dividend} * {divider} = {result}");
+        return result;
+    }
+
+    private double Modulo(double dividend, double divider)
+    {
+        var result = dividend % divider;
+        _logger.LogInformation($"Остаток от деления: {dividend} % {divider} = {result}");
+        return result;
+    }
+
+    private double Exponentiation(double dividend, double divider)
+    {
+        var result = Math.Pow(dividend, divider);
+        _logger.LogInformation($"Возведение в степень: {dividend} ^ {divider} = {result}");
+        return result;
+    }
 }
---
+++ Калькулятор/Логгер/MyNewLogger.cs
@@ -0,0 +1,68 @@
+﻿namespace Логгер;
+
+public interface ILogger
+{
+   void LogInformation(string message);
+   void LogError(Exception exception, string? additionalMessage = null);
+}
+
+public class ConsoleLogger : ILogger
+{
+   public void LogInformation(string message)
+   {
+      Console.WriteLine(message);
+   }
+
+   public void LogError(Exception exception, string? additionalMessage = null)
+   {
+      Console.WriteLine(exception.Message);
+      if (additionalMessage != null)
+      {
+         Console.WriteLine(additionalMessage);
+      }
+   }
+}
+
+public class FileLogger : ILogger
+{
+   public void LogInformation(string message)
+   {
+      using var sw = File.AppendText("log.txt");
+      sw.WriteLine(message);
+   }
+
+   public void LogError(Exception exception, string? additionalMessage = null)
+   {
+      using var sw = File.AppendText("log.txt");
+      sw.WriteLine(exception.Message);
+      if (additionalMessage != null)
+      {
+         sw.WriteLine(additionalMessage);
+      }
+   }
+}
+
+public class CompositeLogger : ILogger
+{
+   private readonly List<ILogger> _loggers;
+   public CompositeLogger(List<ILogger> loggers)
+   {
+      _loggers = loggers;
+   }
+
+   public void LogInformation(string message)
+   {
+      foreach (var logger in _loggers)
+      {
+         logger.LogInformation(message);
+      }
+   }
+
+   public void LogError(Exception exception, string? additionalMessage = null)
+   {
+      foreach (var logger in _loggers)
+      {
+         logger.LogError(exception, additionalMessage);
+      }
+   }
+}
